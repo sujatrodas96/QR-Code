28e4d754a03de702f7ed9ab38e0325d6d9143e3b
sujatrodas96 <sujatro.das96@gmail.com>
2025-06-01 13:10:53 +0530

    new changes

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ app.get('/', (req, res) => {
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
         rel="stylesheet"
       />
+      <style>
+      .card {
+        max-width: 500px;
+        width: 100%;
+        margin: 0 auto;
+      }
+    </style>
     </head>
     <body class="bg-light">
       <div class="container mt-5">
